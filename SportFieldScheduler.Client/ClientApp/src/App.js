fae428a075ca1955660aab65187135d8928c69a5
Alexandru Nicoara <alexandru.nicoara@amdaris.com>
2022-05-18 17:37:54 +0300

    Repair proxy

--- SportFieldScheduler.Client/ClientApp/src/App.js
+++ SportFieldScheduler.Client/ClientApp/src/App.js
@@ -1,27 +1,26 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import  SportFields  from './components/SportFields';
-import ShowAppointments from './components/ShowAppointments';
-import ShowUsers from './components/ShowUsers';
-import ShowAllSportFields from './components/ShowAllSportFields';
-import AddUserForm from './components/AddUserForm';
-import './custom.css'
-
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import { Home } from "./components/Home";
+import SportFields from "./components/SportFields";
+import ShowAppointments from "./components/ShowAppointments";
+import ShowUsers from "./components/ShowUsers";
+import ShowAllSportFields from "./components/ShowAllSportFields";
+import AddUserForm from "./components/AddUserForm";
+import "./custom.css";
 
 export default class App extends Component {
   static displayName = App.name;
 
-  render () {
+  render() {
     return (
       <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path ='/sport-fields' component={SportFields}/>
-        <Route path='/show-all-appointments' component={ShowAppointments}/>
-        <Route path='/show-all-users' component={ShowUsers}/>
-        <Route path='/show-all-sport-fields' component={ShowAllSportFields}/>
-        <Route path ='/add-user-form' component={AddUserForm}/>
+        <Route exact path="/" component={Home} />
+        <Route path="/sport-fields" component={SportFields} />
+        <Route path="/show-all-appointments" component={ShowAppointments} />
+        <Route path="/show-all-users" component={ShowUsers} />
+        <Route path="/show-all-sport-fields" component={ShowAllSportFields} />
+        <Route path="/add-user-form" component={AddUserForm} />
       </Layout>
     );
   }
